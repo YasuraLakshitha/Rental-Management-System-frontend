234f2f47f99e7ba38486a1adfd152a71fcded9c6
Yasura Lakshitha <lakshitha200087@gmail.com>
2024-07-26 15:30:45 +0530

    implementation rental component and crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
+import {AddItemComponent} from "./components/add-item/add-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+      RouterOutlet,
+      NavBarComponent,
+      AddItemComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {AddItemComponent} from "./components/add-item/add-item.component";
+import {ItemTableComponent} from "./components/item-table/item-table.component";
+import {AddRentalComponent} from "./components/add-rental/add-rental.component";
+import {AllRentalsComponent} from "./components/all-rentals/all-rentals.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:AddItemComponent
+    },
+    {
+        path: 'add-item',
+        component: AddItemComponent
+    },
+    {
+        path: 'item-table',
+        component:ItemTableComponent
+    },
+    {
+        path: 'add-rental',
+        component: AddRentalComponent
+    },
+    {
+        path: 'rental-table',
+        component: AllRentalsComponent
+    }
+];
